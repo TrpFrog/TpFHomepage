128aeaa63a2abc63fbe7ca058a9f712f8aff8649
つまみ <mtomo@me.com>
2019-11-23 12:36:53 +0900

    フッターの範囲を中身のブロックがオーバーしていた問題を修正

--- new_contents_builder.js
+++ new_contents_builder.js
@@ -1,14 +1,12 @@
 function fontLoader() {
-    let html = "";
-    html = `
+    let html = `
         <link href="https://fonts.googleapis.com/css?family=Comfortaa|M+PLUS+Rounded+1c:400,800|Questrial|Noto+Sans+JP&display=swap&subset=japanese" rel="stylesheet">
     `;
     document.write(html);
 }
 
 function buildHeader() {
-    let html = "";
-    html = `
+    let html = `
         <h1>つまみネット</h1>
         <nav>
             <ul>
@@ -22,16 +20,14 @@ function buildHeader() {
 }
 
 function buildFooter() {
-    let html = "";
-    //`+url+`
-    html = `
+    let html = `
         <p id="copyright">
             ©︎ 2019 つまみ
         </p>
         <p id="share">
-            <a href="`+getTwitterUrl()+`"><img src="images/socialicons/twitter.svg" height="45" class="shareButton"></a>
-            <a href="`+getFacebookUrl()+`"><img src="images/socialicons/facebook.svg" height="45" class="shareButton"></a>
-            <a href="`+getLineUrl()+`"><img src="images/socialicons/line.svg" height="45" class="shareButton"></a>
+            <a href="`+getTwitterUrl()+`"><img src="images/socialicons/twitter.svg" height="45" class="shareButton" alt="Share with Twitter"></a>
+            <a href="`+getFacebookUrl()+`"><img src="images/socialicons/facebook.svg" height="45" class="shareButton" alt="Share with Facebook"></a>
+            <a href="`+getLineUrl()+`"><img src="images/socialicons/line.svg" height="45" class="shareButton" alt="Share with LINE"></a>
         </p>
     `;
     document.write(html);
