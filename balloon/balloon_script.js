29915fc37fb5d3c406abed719a1a5034c2d8efea
つまみ <mtomo@me.com>
2020-02-28 09:58:29 +0900

    大規模アップデート
    コミットし忘れたので変更点がヤバいですねっ⭐️

--- balloon/balloon_script.js
+++ balloon/balloon_script.js
@@ -76,3 +76,33 @@ function buildBalloons(){
     document.write(html);
   }
 }
+
+function buildTopBalloons(){
+  for(let i = 0; i<balloonClicked.length; i++){
+    let random = Math.floor( Math.random() * 3 );
+    let color = "orange";
+    switch (random) {
+      case 0:
+        color = "orange";
+        break;
+      case 1:
+        color = "blue";
+        break;
+      case 2:
+        color = "green";
+        break;
+      default:
+    }
+    let html = '';
+    html += '<img src=\"'+b_homedir+'/images/balloon/'+color+'/normal.png\" '
+    html += 'width=25%'
+    html += 'onmouseover=\"if( !balloonClicked['+i+'] ) this.src=\''+b_homedir+'/images/balloon/'+color+'/tremble.gif\'\" '
+    html += 'onmouseout=\"if( !balloonClicked['+i+'] ) this.src=\''+b_homedir+'/images/balloon/'+color+'/normal.png\'\" '
+    html += 'onclick=\"this.src=\''+b_homedir+'/images/balloon/'+color+'/break.png\'; '
+    html += '    if( !balloonClicked['+i+'] ) breakBalloon();'
+    html += '    balloonClicked['+i+'] = true\"'
+    html += 'style=\"cursor: crosshair\"; '
+    html += 'class=\"balloon\">'
+    document.write(html);
+  }
+}
