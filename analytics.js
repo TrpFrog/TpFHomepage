947928fb9927a2ac6d9c7097ebd4d0f4828b5df6
つまみ <mtomo@me.com>
2020-09-22 22:25:15 +0900

    Replace analytics tags with analytics.js

---
+++ analytics.js
@@ -0,0 +1,5 @@
+document.write('<script async src = "https://www.googletagmanager.com/gtag/js?id=UA-144514287-1"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'UA-144514287-1');
