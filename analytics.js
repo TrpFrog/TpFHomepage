3618dd28acc07d545f276f0bb0183da291cd8098
つまみ <dev@trpfrog.net>
2021-04-13 20:50:41 +0900

    Add AdSense tags

--- analytics.js
+++ analytics.js
@@ -3,3 +3,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 gtag('config', 'UA-144514287-1');
+document.write('<script data-ad-client="ca-pub-3619599723921639" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
