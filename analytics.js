079d641dd0632ae07fcb74e502c73b209921d38a
TrpFrog <dev@trpfrog.net>
2021-07-31 16:43:34 +0900

    Remove Google Analytics

--- analytics.js
+++
@@ -1,6 +0,0 @@
-document.write('<script async src = "https://www.googletagmanager.com/gtag/js?id=UA-144514287-1"></script>');
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'UA-144514287-1');
-document.write('<script data-ad-client="ca-pub-3619599723921639" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
