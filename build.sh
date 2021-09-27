a8d8d14be09f792b46211586c73e964a31424c0e
TrpFrog <dev@trpfrog.net>
2021-09-27 13:42:45 +0900

    Change build command

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+python3 -m pip install markdown
+python3 -m pip install beautifulsoup4
+
 cd notes
-/opt/buildhome/python3.7/bin/python3.7 -m pip install markdown
-/opt/buildhome/python3.7/bin/python3.7 -m pip install beautifulsoup4
-/opt/buildhome/python3.7/bin/python3.7 notes_builder.py
+python3 notes_builder.py
