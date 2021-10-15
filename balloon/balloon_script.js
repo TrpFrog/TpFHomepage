d38f5b6924a2ea2a908b9bad302af71593ccaa43
TrpFrog <dev@trpfrog.net>
2021-10-15 23:21:01 +0900

    Fix balloons on the top page not to tremble

--- balloon/balloon_script.js
+++ balloon/balloon_script.js
@@ -1,8 +1,8 @@
 let balloonClicked = Array(5);
 reset();
 let breakSound = false;
-let urlpara = location.search;
-if (urlpara == "?enable_breaking_sound") {
+let urlParameter = location.search;
+if (urlParameter === "?enable_breaking_sound") {
     breakSound = true;
 }
 let audio = document.getElementById('sound');
@@ -11,17 +11,13 @@ let b_homedir = 'https://trpfrog.net';
 let b_width = 50;
 
 function buildSoundButton() {
-    let html = '';
+    let html;
     if (breakSound) {
         html = '<a href=\"' + b_homedir + '/balloon/index.html\" class=\"linkButton\">音を消す</a>';
     } else {
         html = '<a href=\"' + b_homedir + '/balloon/index.html?enable_breaking_sound\" class=\"linkButton\">音を鳴らす</a>';
     }
     document.getElementById('title').innerHTML += html;
-}
-
-function setHomedir(newhomedir) {
-    b_homedir = newhomedir;
 }
 
 function setWidth(newwidth) {
@@ -88,19 +84,19 @@ function buildBalloons() {
 }
 
 function buildTopBalloons(preparedBaloons) {
-    let html = '';
-    let prevcolor = '';
-    let colorhistory = [];
+    let previousColorIndex = 0;
+    let colorHistory = [];
     let isCompleted = true;
+    let html = '';
     for (let i = 0; i < balloonClicked.length; i++) {
         let random = Math.floor(Math.random() * 3);
         
-        if(i == 0){
-            prevcolor = random;
+        if(i === 0){
+            previousColorIndex = random;
         }else{
-            if(prevcolor != random) isCompleted = false;
+            if(previousColorIndex !== random) isCompleted = false;
         }
-        colorhistory.push(random);
+        colorHistory.push(random);
 
         let color = "orange";
         switch (random) {
@@ -115,26 +111,33 @@ function buildTopBalloons(preparedBaloons) {
                 break;
             default:
         }
-        html += '<img src=\"' + b_homedir + '/images/balloon/' + color + '/normal.png\" '
-        html += 'width='
-        html += (100 / preparedBaloons) + ''
-        html += '%'
-        html += 'onmouseover=\"if( !balloonClicked[' + i + '] ) this.src=\'' + b_homedir + '/images/balloon/' + color + '/tremble.gif\'\" '
-        html += 'onmouseout=\"if( !balloonClicked[' + i + '] ) this.src=\'' + b_homedir + '/images/balloon/' + color + '/normal.png\'\" '
-        html += 'onclick=\"this.src=\'' + b_homedir + '/images/balloon/' + color + '/break.png\'; '
-        html += '    if( !balloonClicked[' + i + '] ){ breakBalloon();'
-        html += '    incrementStatNumber(BALLOON_BROKENS);}'
-        html += '    balloonClicked[' + i + '] = true\"'
-        html += 'style=\"cursor: crosshair\"; '
-        html += 'class=\"balloon\">'
+        html += `
+            <img src="/images/balloon/${color}/normal.png"
+                alt="${color} balloon" 
+                width="${Math.floor(100 / preparedBaloons) - 0.5}%" 
+                onmouseover="
+                    if( !balloonClicked[${i}] ) this.src = '/images/balloon/${color}/tremble.gif';
+                " 
+                onmouseout="
+                    if( !balloonClicked[${i}] ) this.src='/images/balloon/${color}/normal.png'
+                " 
+                onclick="this.src='/images/balloon/${color}/break.png';
+                    if( !balloonClicked[${i}] ) {
+                        breakBalloon();
+                        incrementStatNumber(BALLOON_BROKENS);
+                    }
+                    balloonClicked[${i}] = true;
+                "
+                style="cursor: crosshair" 
+                class="balloon">`;
     }
     if(isCompleted) {
         incrementStatNumber(BALLOON_COMPLETED);
     }
 
     let isPalindrome = true;
-    for(let i = 0; i < 3; i++){
-        if(colorhistory[i] != colorhistory[6-i]) isPalindrome = false;
+    for(let i = 0; i < preparedBaloons / 2; i++){
+        if(colorHistory[i] !== colorHistory[preparedBaloons - 1 - i]) isPalindrome = false;
     }
     if (isPalindrome) {
         incrementStatNumber(BALLOON_SYMMETRY);
