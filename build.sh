3ead568812a53e21fe4d44ad47dfedc9afb65e3c
TrpFrog <dev@trpfrog.net>
2021-12-21 12:27:09 +0900

    Update build.sh for Vercel

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cd notes
-/opt/buildhome/python3.7/bin/python3.7 -m pip install markdown
-/opt/buildhome/python3.7/bin/python3.7 -m pip install beautifulsoup4
-/opt/buildhome/python3.7/bin/python3.7 notes_builder.py
+cd builder
+python3 notes_builder.py
